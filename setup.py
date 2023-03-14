d0f09c45b119be521fe468117a77afd1fbd1bac3
shaheerpvm <shaheerpvm@gmail.com>
2023-03-14 14:35:18 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+name='modelling_welding',
+version='0.0.1',
+author='shaheer',
+author_email='shaheerpvm@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
